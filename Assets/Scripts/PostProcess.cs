725a51f7edd3324e56a7b72141e92a3dd3188a87
Hugo Serinese (RIT Student) <hhs2791@rit.edu>
2022-10-28 18:30:46 -0400

    Materials, Scripts and Shaders add

---
+++ Assets/Scripts/PostProcess.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PostProcess : MonoBehaviour
+{
+
+    public Material material;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
